5bab521a6e66d64df640cab3a9594b07e866c1b1
Ivan Iziumov <iiziumov@anaconda.com>
2025-09-24 13:47:15 +0200

    libosqp rebuild

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,13 @@
 @echo on
 
+:: Configure.
 cmake -G "Ninja" -B build -S . ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DENABLE_MKL_PARDISO=OFF ^
-    -DOSQP_RESPECT_BUILD_SHARED_LIBS:BOOL=ON ^
-    -DBUILD_SHARED_LIBS=ON ^
+    -DOSQP_BUILD_STATIC_LIB=ON ^
     -DOSQP_BUILD_UNITTESTS=ON
     
 if %ERRORLEVEL% neq 0 exit 1
@@ -20,6 +20,22 @@ if %ERRORLEVEL% neq 0 exit 1
 cmake --build build --target test
 if %ERRORLEVEL% neq 0 exit 1
 
+:: Reconfigure.
+cmake -G "Ninja" -B build-nostatic -S . ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    -DENABLE_MKL_PARDISO=OFF ^
+    -DOSQP_RESPECT_BUILD_SHARED_LIBS:BOOL=ON ^
+    -DOSQP_BUILD_SHARED_LIB=ON ^
+    -DOSQP_BUILD_STATIC_LIB=OFF
+if %ERRORLEVEL% neq 0 exit 1
+
+:: Rebuild.
+cmake --build build-nostatic -j
+if %ERRORLEVEL% neq 0 exit 1
+
 :: Install.
-cmake --install build
+cmake --install build-nostatic
 if %ERRORLEVEL% neq 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,10 +8,22 @@ cmake -G Ninja -B build -S . \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DENABLE_MKL_PARDISO=OFF \
-    -DOSQP_RESPECT_BUILD_SHARED_LIBS:BOOL=ON \
-    -DBUILD_SHARED_LIBS=ON \
+    -DOSQP_BUILD_STATIC_LIB=ON \
     -DOSQP_BUILD_UNITTESTS=ON
 
 cmake --build build -j
 cmake --build build --target test
-cmake --install build
+
+cmake -G Ninja -B build-nostatic -S . \
+    ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DENABLE_MKL_PARDISO=OFF \
+    -DOSQP_RESPECT_BUILD_SHARED_LIBS:BOOL=ON \
+    -DOSQP_BUILD_SHARED_LIB=ON \
+    -DOSQP_BUILD_STATIC_LIB=OFF
+
+cmake --build build-nostatic -j
+cmake --install build-nostatic
